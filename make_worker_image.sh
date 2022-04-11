f5c18a9e241b536494ce7265e36bd4129e6177f0
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-04-11 10:53:34 -0400

    edits

--- make_worker_image.sh
+++ make_worker_image.sh
@@ -4,11 +4,11 @@
 
 GIT_COMMIT="$(git show --format="%h" --no-patch)"
 
-docker run --rm --privileged
-    -e PIO_VERSION=$1
-    -e CUSTOPIZER_GIT_COMMIT=GIT_COMMIT
-    -e WORKER=1
-    -e LEADER=0
+docker run --rm --privileged \
+    -e PIO_VERSION=$1 \
+    -e CUSTOPIZER_GIT_COMMIT=GIT_COMMIT \
+    -e WORKER=1 \
+    -e LEADER=0 \
     -v /Users/camerondavidson-pilon/code/CustoPiZer/workspace:/CustoPiZer/workspace/  -v /Users/camerondavidson-pilon/code/CustoPiZer/config.local:/CustoPiZer/config.local ghcr.io/octoprint/custopizer:latest \
  && zip workspace/pioreactor_worker.img.zip workspace/output.img \
  && rm workspace/output.img
