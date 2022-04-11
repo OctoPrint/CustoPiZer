eefed665ddf6fd394e4980f53395335b11334960
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-04-11 09:49:41 -0400

    change comparison; adding a build metadata file

--- make_leader_worker_image.sh
+++ make_leader_worker_image.sh
@@ -2,6 +2,13 @@
 
 : ${1?"Usage: $0 PIO_VERSION"}
 
-docker run --rm --privileged -e PIO_VERSION=$1 -e WORKER=1 -e LEADER=1 -v /Users/camerondavidson-pilon/code/CustoPiZer/workspace:/CustoPiZer/workspace/  -v /Users/camerondavidson-pilon/code/CustoPiZer/config.local:/CustoPiZer/config.local ghcr.io/octoprint/custopizer:latest \
+GIT_COMMIT="$(git show --format="%h" --no-patch)"
+
+docker run --rm --privileged
+    -e PIO_VERSION=$1
+    -e CUSTOPIZER_GIT_COMMIT=GIT_COMMIT
+    -e WORKER=1
+    -e LEADER=1
+    -v /Users/camerondavidson-pilon/code/CustoPiZer/workspace:/CustoPiZer/workspace/  -v /Users/camerondavidson-pilon/code/CustoPiZer/config.local:/CustoPiZer/config.local ghcr.io/octoprint/custopizer:latest \
  && zip workspace/pioreactor_leader_worker.img.zip workspace/output.img \
  && rm workspace/output.img
