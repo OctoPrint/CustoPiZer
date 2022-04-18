b0eeac2ae11b45da88b7f5224b3e4decd498cd17
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-04-18 15:00:41 -0400

    this works, and turns on and off using GPIO

--- workspace/scripts/files/bash/conditionally_start_raspap.sh
+++ workspace/scripts/files/bash/conditionally_start_raspap.sh
@@ -6,11 +6,19 @@ set -e
 export LC_ALL=C
 
 
-WPA_FILE=/etc/wpa_supplicant
+WPA_FILE=/etc/wpa_supplicant/wpa_supplicant.conf
+RASPAP_TRIGGER_ON_PIN=20
+RASPAP_TRIGGER_OFF_PIN=26
 
-# If there are no WIFI credentials, or GPIO is pulled HIGH, then start the raspap service
-if [ -f $WPA_FILE ] && [ $(raspi-gpio get 20 | cut -d " " -f 3) == "level=0" ]; then
-    sudo systemctl disable raspapd.service
-else
-    sudo systemctl enable raspapd.service
+
+# If GPIO 26 is pulled HIGH, then stop and disable the RaspAP access point
+if [[ $(raspi-gpio get $RASPAP_TRIGGER_OFF_PIN | cut -d " " -f 3) == "level=1" ]]; then
+    sudo systemctl disable raspapd.service --now
+    sudo systemctl disable hostapd.service --now
+fi
+
+# If there are no WIFI credentials, or GPIO 20 is pulled HIGH, then enable and start the RaspAP access point
+if [[ (! -f $WPA_FILE) || $(raspi-gpio get $RASPAP_TRIGGER_ON_PIN | cut -d " " -f 3) == "level=1" ]]; then
+    sudo systemctl enable raspapd.service --now
+    sudo systemctl enable hostapd.service --now
 fi
