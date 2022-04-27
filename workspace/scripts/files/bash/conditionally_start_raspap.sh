9b69bb2b452512438d63c23c474e269f093c3a08
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-04-27 17:49:54 -0400

    adding simple pio logging to services

--- workspace/scripts/files/bash/conditionally_start_raspap.sh
+++ workspace/scripts/files/bash/conditionally_start_raspap.sh
@@ -11,31 +11,31 @@ RASPAP_TRIGGER_ON_PIN=20
 RASPAP_TRIGGER_OFF_PIN=26
 
 
-function swap()
-{
-    local TMPFILE=tmp.$$
-    mv "$1" $TMPFILE && mv "$2" "$1" && mv $TMPFILE "$2"
-}
 
-
-# If GPIO 26 is pulled HIGH, then stop and disable the RaspAP access point
-if [[ $(raspi-gpio get $RASPAP_TRIGGER_OFF_PIN | cut -d " " -f 3) == "level=1" ]]; then
+# If GPIO 26 is pulled HIGH, then stop and disable the RaspAP access point, but only if there are WIFI credentials to use
+if [[ (-f $WPA_FILE) && $(raspi-gpio get $RASPAP_TRIGGER_OFF_PIN | cut -d " " -f 3) == "level=1" ]]; then
     # only execute if is enabled
-    if [[ $(systemctl is-enabled --quiet hostapd.service) ]]; then
-        swap /etc/raspap/backups/dhcpcd.conf /etc/dhcpcd.conf
-        sudo systemctl restart dhcpcd.service
-        sudo systemctl disable raspapd.service --now
-        sudo systemctl disable hostapd.service --now
+    if [[ $(systemctl is-enabled hostapd.service) == "enabled" ]]; then
+        # this order seems to be important.
+        systemctl disable raspapd.service --now
+        systemctl disable hostapd.service --now
+        cp /etc/raspap/backups/dhcpcd.conf.original /etc/dhcpcd.conf
+        systemctl daemon-reload
+        systemctl restart wpa_supplicant.service
+        systemctl restart dhcpcd.service
+        pio append_to_log -m "Turning off hotspot"
     fi
 fi
 
 # If there are no WIFI credentials, or GPIO 20 is pulled HIGH, then enable and start the RaspAP access point
 if [[ (! -f $WPA_FILE) || $(raspi-gpio get $RASPAP_TRIGGER_ON_PIN | cut -d " " -f 3) == "level=1" ]]; then
     # only execute if not enabled
-    if [[ ! $(systemctl is-enabled --quiet hostapd.service) ]]; then
-        swap /etc/raspap/backups/dhcpcd.conf /etc/dhcpcd.conf
-        sudo systemctl restart dhcpcd.service
-        sudo systemctl enable raspapd.service --now
-        sudo systemctl enable hostapd.service --now
+    if [[ $(systemctl is-enabled hostapd.service) == "disabled" ]]; then
+        cp /etc/raspap/backups/dhcpcd.conf.raspap /etc/dhcpcd.conf
+        systemctl daemon-reload
+        systemctl restart dhcpcd.service
+        systemctl enable raspapd.service --now
+        systemctl enable hostapd.service --now
+        pio append_to_log -m "Turning on hotspot"
     fi
 fi
