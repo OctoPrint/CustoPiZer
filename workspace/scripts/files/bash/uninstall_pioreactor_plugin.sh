788b0f3f907795089ba4c13c5df2e6302b56fb00
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-09-02 13:43:20 -0400

    partial resolve https://github.com/Pioreactor/pioreactorui/issues/106

--- workspace/scripts/files/bash/uninstall_pioreactor_plugin.sh
+++ workspace/scripts/files/bash/uninstall_pioreactor_plugin.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # arg1 is the name of the plugin to install
-set -e
+set +e
 set -x
 export LC_ALL=C
 
 plugin_name=$1
+
+
+# the below can fail, and will fail on a worker
+# delete yamls from pioreactorui
+plugin_name_with_underscores=${plugin_name//-/_}
+(cd /usr/local/lib/python3.9/dist-packages/pioreactor_air_bubbler/ui/contrib/ && find * -type f) | awk '{print "/home/pioreactor/pioreactorui/backend/contrib/"$1}' | xargs rm
+
+# remove config.ini
+# TODO. crudini isn't much help here.
 
 
 sudo pip3 uninstall  --root-user-action=ignore  -y $plugin_name
