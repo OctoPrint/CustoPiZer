16d9378377b8c7ce49e0a9118fda9d9c1bc47b83
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-09-14 21:12:15 -0400

    more

--- workspace/scripts/08-install-pioreactorui.sh
+++ workspace/scripts/08-install-pioreactorui.sh
@@ -11,18 +11,25 @@ UI_FOLDER=/var/www/pioreactor
 
 if [ "$LEADER" == "1" ]; then
 
+    # install lighttp and set up mods
     apt-get install lighttpd
-    cp /files/lighttpd/50-pioreactorui.conf etc/lighttpd/conf-available/50-pioreactorui.conf
+    cp /files/lighttpd/50-pioreactorui.conf /etc/lighttpd/conf-available/50-pioreactorui.conf
 
     lighttpd-enable-mod fastcgi
     lighttpd-enable-mod pioreactorui
+
 
     mkdir $UI_FOLDER
 
     # get latest pioreactorUI code from Github.
     git clone https://github.com/Pioreactor/pioreactorui_backend.git $UI_FOLDER  --depth 1
+    # install the dependencies
+    pip3 install -r $UI_FOLDER/requirements.txt
+
+    # init .env
     mv $UI_FOLDER/.env.example $UI_FOLDER/.env
 
+    # set up permissions so www-data can read these files
     chmod +x $UI_FOLDER/app.fcgi # TODO
     chown -R www-data:www-data $UI_FOLDER
 
