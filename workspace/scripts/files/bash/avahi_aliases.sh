08809177d2a0739059fc1cab215d012511955a97
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-09-21 09:43:05 -0400

    test

--- workspace/scripts/files/bash/avahi_aliases.sh
+++ workspace/scripts/files/bash/avahi_aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOMAIN_ALIAS=$(crudini --get /home/pioreactor/.pioreactor/config.ini ui domain_alias)
-IP=$(avahi-resolve -4 -n $(hostname).local | cut -f 2)
+IP=$(hostname -I | awk '{print $1}')
 
-/usr/bin/avahi-publish -a -R        $DOMAIN_ALIAS $IP &
-/usr/bin/avahi-publish -a -R raspap.$DOMAIN_ALIAS $IP &
+/usr/bin/avahi-publish -a -R        "$DOMAIN_ALIAS" "$IP" &
+/usr/bin/avahi-publish -a -R "raspap.$DOMAIN_ALIAS" "$IP" &
